3187d5a5867a5e725427a37fb1b133c007803515
Luis Corales <101592507+LuisCoralesM@users.noreply.github.com>
2025-05-18 22:18:48 -0500

    test

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,24 @@
-import { Router } from "express";
+import cors from "cors";
+import express, { Router } from "express";
 import { v1Router } from "./v1/index.js";
 
+const port = process.env.PORT || 8000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 const apiRouter = Router();
-
 apiRouter.use("/v1", v1Router);
+app.use(apiRouter);
 
-export { apiRouter };
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log("Server running on port", port);
+  console.log(`http://localhost:${port}`);
+});
+
+export default app;
