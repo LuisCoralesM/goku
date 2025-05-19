49a400a7420f32bcd1ad3ce7753def3ca60e9452
Luis Corales <101592507+LuisCoralesM@users.noreply.github.com>
2025-05-18 21:33:03 -0500

    test

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,12 @@
 import cors from 'cors';
-import express from 'express';
+import express, { json } from 'express';
 import { apiRouter } from './api';
 
 const port = process.env.PORT || 8000;
 
 const app = express();
 app.use(cors());
-// app.use(json());
+app.use(json());
 
 app.use('/api', apiRouter);
 
